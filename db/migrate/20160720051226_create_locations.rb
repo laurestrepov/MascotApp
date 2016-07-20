020ddd8d898d82400d2d132dbcc0f8d7b5923a52
LAURESTREPOV <lau.restrepo.v@gmail.com>
2016-07-20 00:19:06 -0500

    Model migrations for User, Company and location

---
+++ db/migrate/20160720051226_create_locations.rb
@@ -0,0 +1,10 @@
+class CreateLocations < ActiveRecord::Migration
+  def change
+    create_table :locations do |t|
+      t.float :location_latitude
+      t.float :location_longitude
+
+      t.timestamps null: false
+    end
+  end
+end
